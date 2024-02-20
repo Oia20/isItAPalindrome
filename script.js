4edd560a3c8cf620eb43905e50b6c878971f3c33
Jacob <92279567+Oia20@users.noreply.github.com>
2024-02-20 14:58:40 -0600

    Initializing project into github, functionality finished, now just need to add basic styling.

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.getElementById('text-input')
+const submit = document.getElementById("check-btn")
+const result = document.getElementById("result")
+function checkTextEntry () {
+  if (input.value === '') {
+    alert("Please input a value")
+  } else {
+    checkIfPalindrome()
+  }
+}
+
+const checkIfPalindrome = () => {
+  let dealWithThisInput = input.value
+  dealWithThisInput = dealWithThisInput.replace(/\s/g, '');
+  let inputWithoutSpecialCharacters = dealWithThisInput.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+  let listToBeReversed = inputWithoutSpecialCharacters.split("")
+  let listReversed = listToBeReversed.reverse()
+  let inputWithoutSpecialCharactersReversed = listReversed.join("")
+  console.log("Regular: ", inputWithoutSpecialCharacters, " Reversed: ", inputWithoutSpecialCharactersReversed)
+  if (inputWithoutSpecialCharacters.toLowerCase() === inputWithoutSpecialCharactersReversed.toLowerCase()) {
+    result.innerHTML = `${input.value} is a palindrome`
+  } else {
+    result.innerHTML = `${input.value} is not a palindrome`
+
+  }
+}
+
+submit.addEventListener('click', checkTextEntry)
+
